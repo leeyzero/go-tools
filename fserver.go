76a3116c163bc5ded3f7948e6b24fce01a3497a4
leeyzero <leeyzero@163.com>
2021-07-29 23:47:40 +0800

    add fserver.go

---
+++ fserver.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+var (
+	port      string
+	directory string
+)
+
+func main() {
+	flag.Parse()
+
+	log.Printf("fserver listen on port:%v, root:%v", port, directory)
+	log.Fatal(http.ListenAndServe(":"+port, http.FileServer(http.Dir(directory))))
+}
+
+func init() {
+	flag.StringVar(&port, "p", "8888", "fserver listen port")
+	flag.StringVar(&directory, "d", ".", "fserver root directory")
+}
